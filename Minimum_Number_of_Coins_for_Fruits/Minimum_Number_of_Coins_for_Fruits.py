cfe8ee6cdd285e11e6b9deb07b1c27aaf783e3a5
johnaopedro <87997969+johnaopedro@users.noreply.github.com>
2024-09-09 19:26:15 -0300

    CODIGO FINAL
    CODIGO FINAL

--- Minimum_Number_of_Coins_for_Fruits/Minimum_Number_of_Coins_for_Fruits.py
+++ Minimum_Number_of_Coins_for_Fruits/Minimum_Number_of_Coins_for_Fruits.py
@@ -1,17 +1,28 @@
 class Solution:
-    def minimumCoins(self, prices: List[int]) -> int:
-        
+    def minimumCoins(self, prices):
+        n = len(prices)
+        current = [0] * (n + 1)
+        current[0] = 0
+        current[1] = prices[0]
+        for i in range(2, n + 1):
+            current[i] = current[i - 1] + prices[i - 1]
+            for j in range(i - 1, 0, -1):
+                if 2 * j < i:
+                    break
+                current[i] = min(current[i], current[j - 1] + prices[j - 1])
+        return current[n]
 
-
-#MEMOIZATION BASE (TIRADO DO EXEMPLO DO SLIDE)
-#Input: n, s1,…,sn , f1,…,fn , v1,…,vn
-#Sort jobs by finish times so that f1 f2 ... fn.
-#Compute p(1), p(2), …, p(n)
-#M[0] = 0
-#for j = 1 to n
-# M[j] = empty
-#M-Compute-Opt(j) {
-# if (M[j] is empty)
-# M[j] = max(wj + M-Compute-Opt(p(j)), M-Compute-Opt(j-1))
-# return M[j]
+#Sequence Alignment: Value of OPT with Linear Space (TIRADO DO EXEMPLO DO SLIDE)
+#Sequence-Alignment(m, n, x1x2...xm, y1y2...yn, , ) {
+# for i = 0 to m
+# CURRENT[i] = i
+# for j = 1 to n
+# LASTCURRENT ( vector copy)
+# CURRENT[0] j
+# for i = 1 to m
+# CURRENT[i]  min( [xi, yj] + LAST[i-1],
+# + LAST[i],
+# + CURRENT[i-1] )
+#return CURRENT[m]
 #}
+
